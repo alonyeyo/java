da2a2416bcac00273ff8618a2a605778da12e5d3
alonyeyo <cabanero.otero@gmail.com>
2015-10-03 19:40:38 +0200

    Mi primera ventana

---
+++ IA atiende/src/Main/IA.java
@@ -0,0 +1,34 @@
+package Main;
+
+import javax.swing.JFrame;
+
+public class IA {
+
+	public static void main(String[] args) {
+		atiende();
+		//ventana
+		Ventana v = new Ventana();
+		v.setVisible(true);
+		v.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+
+	}
+	
+		public static void atiende() {
+			System.out.println("IA está atendiendo!!!");
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+
+}
---
+++ IA atiende/src/Main/Ventana.java
@@ -0,0 +1,13 @@
+package Main;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Ventana extends JFrame{
+	
+	public Ventana(){
+		System.out.println("Estoy ejecutando el constructor de ventana");
+		setSize(640, 480);
+	}
+
+}
